4e180a8b75065dfd696ad410fbd3581785c03af3
15290032120 <185653517@qq.com>
2019-06-10 17:28:31 +0800

    测试页面

---
+++ src/AfarsoftResourcePlan.Web.Mvc/Controllers/FrontEndTestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AfarsoftResourcePlan.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AfarsoftResourcePlan.Web.Mvc.Controllers
+{
+    public class FrontEndTestController : AfarsoftResourcePlanControllerBase
+    {
+        public IActionResult CustomerIndex()
+        {
+            return View();
+        }
+        public IActionResult OrganizationIndex()
+        {
+            return View();
+        }
+        public IActionResult UsersIndex()
+        {
+            return View();
+        }
+        public IActionResult WarehouseIndex()
+        {
+            return View();
+        }
+        public IActionResult SupplierIndex()
+        {
+            return View();
+        }
+        public IActionResult RolesIndex()
+        {
+            return View();
+        }
+        public IActionResult NewSaleOrder()
+        {
+            return View();
+        }
+        public IActionResult SaleOrderList()
+        {
+            return View();
+        }
+    }
+}
