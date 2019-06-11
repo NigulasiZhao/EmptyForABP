69b9b0e45fc7eb06ea9b256ad540c392caf28000
ZL <185653517@qq.com>
2019-06-11 16:09:55 +0800

    枚举转换问题

---
+++ src/AfarsoftResourcePlan.Core/Json/EnumDescriptionJsonConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+
+namespace AfarsoftResourcePlan.Json
+{
+    public class EnumDescriptionJsonConverter : StringEnumConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+            Enum e = (Enum)value;
+
+            writer.WriteValue(GetEnumDescription(e));
+        }
+        private string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            if (attributes != null &&
+                attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+    }
+}
