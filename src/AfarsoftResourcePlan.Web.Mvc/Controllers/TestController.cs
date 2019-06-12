9aa383b28038f96711b3944f157d7d3b98fcd715
renyanwei <78533664@qq.com>
2019-06-12 16:29:52 +0800

    Modified   src/AfarsoftResourcePlan.Web.Mvc/AfarsoftResourcePlan.Web.Mvc.csproj Modified   src/AfarsoftResourcePlan.Web.Mvc/App_Data/Logs/Logs.txt Modified   src/AfarsoftResourcePlan.Web.Mvc/Views/FrontEndTest/CustomerIndex.cshtml Modified   src/AfarsoftResourcePlan.Web.Mvc/Views/FrontEndTest/OrganizationIndex.cshtml Modified   src/AfarsoftResourcePlan.Web.Mvc/wwwroot/view-resources/Views/FrontEndTest/CustomerIndex.js Add        src/AfarsoftResourcePlan.Web.Mvc/Controllers/TestController.cs Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/EditWithTab.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/GoodsList.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/InfoManagerWithDialogAndTab.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/InfoManagerWithDialogWithoutTab.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Order.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Permission.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Query.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Report.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Setting.cshtml Add        src/AfarsoftResourcePlan.Web.Mvc/Views/Test/Track.cshtml

---
+++ src/AfarsoftResourcePlan.Web.Mvc/Controllers/TestController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AfarsoftResourcePlan.Web.Mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        /// <summary>
+        /// 查询类
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Query()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 跟踪类
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Track()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 报表类
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Report()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 带选项卡的编辑页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult EditWithTab()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 基础资料管理（弹窗带选项卡）
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult InfoManagerWithDialogAndTab()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 基础资料管理（弹窗不带选项卡）
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult InfoManagerWithDialogWithoutTab()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 商品列表页（左树右表格）
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GoodsList()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 制单页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Order()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 设置页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Setting()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 权限页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Permission()
+        {
+            return View();
+        }
+    }
+}
