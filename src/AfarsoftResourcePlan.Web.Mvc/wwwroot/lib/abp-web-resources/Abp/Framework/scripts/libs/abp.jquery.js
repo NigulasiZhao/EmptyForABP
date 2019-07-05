312102dfda5202bbf92892f63181a5ace5c75e87
renyanwei <78533664@qq.com>
2019-07-05 14:28:42 +0800

    Modified   src/AfarsoftResourcePlan.Web.Mvc/wwwroot/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.js

--- src/AfarsoftResourcePlan.Web.Mvc/wwwroot/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.js
+++ src/AfarsoftResourcePlan.Web.Mvc/wwwroot/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.js
@@ -81,8 +81,8 @@
         },
 
         showError: function (error) {
-            if (error.details) {
-                return abp.message.error(error.details, error.message);
+            if (error.Details) {
+                return abp.message.error(error.Details, error.Message);
             } else {
                 return abp.message.error(error.message || abp.ajax.defaultError.message);
             }
